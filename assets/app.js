694e83349e7d5c922e4626d049e167253bf13187
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-06-04 23:16:56 -0300

    inicializando carpeta

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+window.onload = function (){
+/*Crea una función que busque un elemento en un array, devolviendo su posición (index) e imprima el valor en la consola. Tip: utiliza el algoritmo de linear search. Con este ejercicio introducimos el concepto de algoritmo */
+
+
+
+}
